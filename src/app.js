789092f15c7a82e18707caac002a45f9d2890969
A Rahman <arsalar286@gmail.com>
2022-11-16 19:55:32 +0500

    NodeJS

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const path=require("path");
 const hbs=require("hbs");
 const app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 8000;
 
 const staticpath =  path.join(__dirname, "../public");
 const template_path =  path.join(__dirname, "../templates/views");
